a463d43101e077d351bb3675d31b52e9953538da
John Cosio <johnc@mybizniche.com>
2024-03-27 17:25:00 +0800

    Update meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "projects.jcos.io",
+    template: "%s | projects.jcos.io",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
-    title: "chronark.com",
+    title: "projects.jcos.io",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://projects.jcos.io",
+    siteName: "projects.jcos.io",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://res.cloudinary.com/langitlupakintoncloud/image/upload/w_800/hugo/jcos.io/logo512x512_ghwawd.png",
         width: 1920,
         height: 1080,
       },
@@ -36,10 +36,6 @@ export const metadata: Metadata = {
       "max-image-preview": "large",
       "max-snippet": -1,
     },
-  },
-  twitter: {
-    title: "Chronark",
-    card: "summary_large_image",
   },
   icons: {
     shortcut: "/favicon.png",
