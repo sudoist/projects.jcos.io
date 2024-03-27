a1376b5f6b033ec5016c9daa97bf282921287a95
John Cosio <johnc@mybizniche.com>
2024-03-27 15:17:34 +0800

    Clean up projects dir and update bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,7 +70,7 @@ export default function RootLayout({
           }`}
       >
 
-        <div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900 ">
+        <div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-yellow-900 ">
           {children}
         </div>
       </body>
