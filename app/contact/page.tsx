6436c1130927a5cee3b4a3f885036520e6ce089c
John Cosio <johnc@mybizniche.com>
2024-03-29 00:31:54 +0800

    Updated contact info

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,22 +6,16 @@ import { Card } from "../components/card";
 
 const socials = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
-	},
-	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:mail@jcos.io",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "mail@jcos.io",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/sudoist",
 		label: "Github",
-		handle: "chronark",
+		handle: "sudoist",
 	},
 ];
 
@@ -30,7 +24,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-2 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
